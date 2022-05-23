311e000a476021ccc34b2dfe8756bb60d2fe3204
peggylicious <p.asq@live.com>
2022-05-23 17:09:11 +0100

    Initialize angular invoice app project and add modules and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InvoiceCardComponent } from './components/invoice-card/invoice-card.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'invoice-card', component: InvoiceCardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/invoice-card/invoice-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-card',
+  templateUrl: './invoice-card.component.html',
+  styleUrls: ['./invoice-card.component.css']
+})
+export class InvoiceCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
