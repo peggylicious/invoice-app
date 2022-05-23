f54de5d28845c7b8b30564d55f8f91c828172976
peggylicious <p.asq@live.com>
2022-05-23 19:11:13 +0100

    Feature: Add button component

---
+++ src/app/shared/components/status-button/status-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-status-button',
+  templateUrl: './status-button.component.html',
+  styleUrls: ['./status-button.component.css']
+})
+export class StatusButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
