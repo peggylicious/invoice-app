3c4e023fa980331ea1244669413285da7b33b894
peggylicious <p.asq@live.com>
2022-05-24 13:02:17 +0100

    Feature: Add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InvoiceCardComponent } from './components/invoice-card/invoice-card.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { InvoiceCardComponent } from './components/invoice-card/invoice-card.com
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatusButtonComponent } from './components/status-button/status-button.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 
 @NgModule({
   declarations: [
-    StatusButtonComponent 
+    StatusButtonComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule
