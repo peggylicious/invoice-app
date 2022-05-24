c2c62922d6aac14ba87e6523059947157a6bedb9
peggylicious <p.asq@live.com>
2022-05-24 22:57:23 +0100

    Feature: Style home page title area

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InvoiceCardComponent } from './components/invoice-card/invoice-card.component';
+import { HomeComponent } from './modules/home/home.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 
 const routes: Routes = [
   {path: 'invoice-card', component: InvoiceCardComponent},
-  {path: 'header', component: HeaderComponent}
+  {path: 'header', component: HeaderComponent}, 
+  {path: 'home', component: HomeComponent}, 
+
 ];
 
 @NgModule({
