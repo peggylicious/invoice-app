77309edce7bc5258d886a72c2461e224c91744d0
peggylicious <p.asq@live.com>
2022-05-24 14:11:09 +0100

    Feature: Style header component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InvoiceCardComponent } from './components/invoice-card/invoice-card.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 const routes: Routes = [
-  {path: 'invoice-card', component: InvoiceCardComponent}
+  {path: 'invoice-card', component: InvoiceCardComponent},
+  {path: 'header', component: HeaderComponent}
 ];
 
 @NgModule({
