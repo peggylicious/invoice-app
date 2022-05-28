b8d1f698d97c8695b35d94395c7a6fd251bf9030
peggylicious <p.asq@live.com>
2022-05-29 00:01:07 +0100

    Feature: add styles for invoice form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { InvoiceCardComponent } from './shared/components/invoice-card/invoice-card.component';
 import { HomeComponent } from './modules/home/home.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import { InvoiceFormComponent } from './shared/components/invoice-form/invoice-form.component';
 
 const routes: Routes = [
   {path: 'invoice-card', component: InvoiceCardComponent},
   {path: 'header', component: HeaderComponent}, 
   {path: 'home', component: HomeComponent}, 
+  {path: 'form', component: InvoiceFormComponent}, 
+
 
 ];
 
