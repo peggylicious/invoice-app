ecd2d50b0864acd8d501867de786de23363b8660
peggylicious <p.asq@live.com>
2022-06-02 16:15:48 +0100

    Feature: Add invoice form to edit invoice component and create route for edit invoice

--- src/app/modules/modify/modify-routing.module.ts
+++ src/app/modules/modify/modify-routing.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateComponent } from './components/create/create.component';
+import { EditInvoiceComponent } from './components/edit-invoice/edit-invoice.component';
 import { ModifyComponent } from './modify.component';
 
 const routes: Routes = [
-  { path: '', component: ModifyComponent, 
-  children: [{ path: 'create', component: CreateComponent }] },
+  {
+    path: '',
+    component: ModifyComponent,
+    children: [
+      { path: 'create', component: CreateComponent },
+      { path: 'edit', component: EditInvoiceComponent },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ModifyRoutingModule { }
+export class ModifyRoutingModule {}
