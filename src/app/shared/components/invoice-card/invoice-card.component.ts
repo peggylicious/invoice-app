d2e685c2137d169dc6c18e4aa0b4a67fb7036f4d
peggylicious <p.asq@live.com>
2022-06-17 15:38:17 +0100

    Use invoice data in invoice card component, and pipe date. While also adding Input to store invoice

--- src/app/shared/components/invoice-card/invoice-card.component.ts
+++ src/app/shared/components/invoice-card/invoice-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-invoice-card',
@@ -8,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class InvoiceCardComponent implements OnInit {
 
   constructor() { }
-
+  @Input() invoice: {} | undefined|any;
+  paymentDue:any;
   ngOnInit(): void {
+    this.changePaymentDue()
   }
+  changePaymentDue(){
+    
+    this.paymentDue = new Date(this.invoice?.paymentDue)
+    console.log(this.paymentDue)
+    return this.paymentDue
+  }
+
+  // public get newDate(){
+  //   return new Date(this.invoice.paymentDue)
+  // }
 
 }
