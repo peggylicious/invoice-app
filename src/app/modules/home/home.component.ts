fbe40b35ee353ed2f8ecb36ae7a2e7e03103f47c
peggylicious <p.asq@live.com>
2022-06-18 20:10:27 +0100

    Add function to navigate to view-invoice component when invoice card is clicked. Ths is inorder to view each invoice in detail

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { InvoiceServiceService } from 'src/app/services/invoice-service.service';
+import { SharedService } from 'src/app/shared/services/shared.service';
+import { Invoice } from 'src/app/shared/interfaces/invoice';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,10 +10,10 @@ import { InvoiceServiceService } from 'src/app/services/invoice-service.service'
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private inv: InvoiceServiceService) { }
+  constructor(private inv: InvoiceServiceService, private sharedservice: SharedService, private route: ActivatedRoute, private router: Router) { }
   buttonState = 'delete'
   buttonTitle = 'delete';
-  invoices = [];
+  invoices: Invoice[] = []
   ngOnInit(): void {
     this.inv.getAllInvoice().subscribe({
       next: (result:any)=>{
@@ -22,7 +25,19 @@ export class HomeComponent implements OnInit {
       }
     })
   }
-  sayHello(){
-    console.log("Hello!!!")
+  openInvoice(id:any){
+    // this.sharedservice.displayInvoice(id)
+    // Check if id exists in array of objects
+    // let name = this.invoices.filter(invoice => {
+    //   if(invoice.id === id) {
+    //     console.log(invoice)
+        this.router.navigateByUrl(`/view-invoice/${id}`)
+        this.route.params.subscribe(param => {
+          console.log(param)
+      })
+    //   }
+      
+    // })
+   
   }
 }
