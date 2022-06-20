c85889b3192a7b7e46c81d624098e2dc738447c6
peggylicious <p.asq@live.com>
2022-06-20 23:14:33 +0100

    Fix functionality issues with switching them

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -13,11 +13,15 @@ export class HeaderComponent implements OnInit {
   }
   switchTheme(state: string){
     if(state === 'dark'){
-      document.querySelectorAll('.dark-root')[0].classList.add('light-root');
-      document.querySelectorAll('.light-root')[0].classList.remove('dark-root');
-    }else{
-      document.querySelectorAll('.light-root')[0].classList.add('dark-root');
-      document.querySelectorAll('.dark-root')[0].classList.remove('light-root');
+      document.querySelectorAll('.theme')[0].classList.add('dark-root');
+      document.querySelectorAll('.theme')[0].classList.remove('light-root');
+      this.theme = 'dark'
+      localStorage.setItem('theme', 'dark')
+    }else if (state === 'light'){
+      document.querySelectorAll('.theme')[0].classList.add('light-root');
+      document.querySelectorAll('.theme')[0].classList.remove('dark-root');
+      this.theme = 'light'
+      localStorage.setItem('theme', 'light')
     }
   }
 
