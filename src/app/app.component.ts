8f128eb3243c4a66a2d22f30d1dbd03ca1dc5665
peggylicious <p.asq@live.com>
2022-06-20 23:19:09 +0100

    Add code to check theme style via localStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'invoice-app';
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.setTheme()
+  }
+  setTheme(){
+    if(localStorage.getItem('theme') === 'dark'){
+      console.log(localStorage.getItem('theme'))
+      document.querySelectorAll('.theme')[0].classList.add('dark-root');
+      // document.querySelectorAll('.theme')[0].classList.remove('dark-root');
+      // localStorage.setItem('theme', 'dark')
+    }else if(localStorage.getItem('theme') === 'light'){
+      document.querySelectorAll('.theme')[0].classList.add('light-root');
+      // document.querySelectorAll('.theme')[0].classList.remove('dark-root');
+      // localStorage.setItem('theme', 'light')
+    }else{
+      document.querySelectorAll('.theme')[0].classList.add(' ');
+    }
+  }
 }
