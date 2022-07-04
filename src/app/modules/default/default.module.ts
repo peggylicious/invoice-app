3000e8165db78e351b5c2e642f1ddbde3804c199
peggylicious <p.asq@live.com>
2022-07-04 22:07:20 +0100

    Add default module components

--- src/app/modules/default/default.module.ts
+++ src/app/modules/default/default.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DefaultComponent } from './default.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DefaultComponent
+  ],
   imports: [
     CommonModule
   ]
