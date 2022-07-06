2f73f15045e383297cf9eb0ac45239f54c6e0122
peggylicious <p.asq@live.com>
2022-07-06 14:37:02 +0100

    Post data to login route

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, FormArray } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private fb: FormBuilder, private auth: AuthService) {}
+  registerForm = this.fb.group({
+    email: [''],
+    password: [''],
+  });
   ngOnInit(): void {
+    this.registerForm.patchValue({
+      email: '',
+      password: '',
+    });
   }
-
+  login() {
+    this.auth.loginUser(this.registerForm.value).subscribe({
+      next: (result) => {
+        console.log(result);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
