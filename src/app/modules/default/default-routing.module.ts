4c4112574aa648d17dcf84c44ddab8dc722f1e47
peggylicious <p.asq@live.com>
2022-07-07 13:28:18 +0100

    Add canactivate dependency injection to home route path

--- src/app/modules/default/default-routing.module.ts
+++ src/app/modules/default/default-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '../auth/guard/auth.guard';
 import { ViewInvoiceComponent } from '../home/components/view-invoice/view-invoice.component';
 import { HomeComponent } from '../home/home.component';
 import { DefaultComponent } from './default.component';
@@ -9,7 +10,7 @@ const routes: Routes = [
     path: '', //Not named so that children routes appear  as desired e.g localhost:4321/home instead of localhost:4321/default/home
     component: DefaultComponent,
     children: [
-      { path: 'home', component: HomeComponent },
+      { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
       { path: 'view-invoice/:id', component: ViewInvoiceComponent },
   { path: 'modify', loadChildren: () => import('../modify/modify.module').then(m => m.ModifyModule) }, //Not named so that children routes appear  as desired e.g localhost:4321/home instead of localhost:4321/default/home
 
