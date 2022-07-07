52ad16a1a2718060c6b664f686eb49276959ff7a
peggylicious <p.asq@live.com>
2022-07-07 13:25:58 +0100

    Activate route if user is loggedIn. Else redirect to login

--- src/app/modules/auth/guard/auth.guard.ts
+++ src/app/modules/auth/guard/auth.guard.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private route: Router, private authService: AuthService){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    if(this.authService.isAuthenticated()){
+      return true
+    }else{
+      this.route.navigateByUrl('/login')
+      return false
+    }
   }
   
 }
