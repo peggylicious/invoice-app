8b7dcdda24fd2019ec89cea7c110a9dad40159dc
peggylicious <p.asq@live.com>
2022-07-07 13:24:07 +0100

    Check if user is still authenticated. i.e. if token lifetime has not expired

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
-
+  constructor(private http: HttpClient, public jwtHelper: JwtHelperService) { }
+  isAuthenticated(): boolean {
+    const token:any = localStorage.getItem('access_token');
+    // Check if token is expired and return
+    // true or false
+    console.log("Helper is ", !this.jwtHelper.isTokenExpired(token))
+    return !this.jwtHelper.isTokenExpired(token);
+    // return false;
+  }
   registerUser(data:any){
     return this.http.post('https://invoice-api-0.herokuapp.com/user/signup', data)
   }
